18f35ad730cd10d1f767ff95d3b8649caa5ce10a
k1k3fy <luisenriqueflemateyanez@gmail.com>
2024-01-24 17:21:39 -0600

    cambios

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import { Route, Routes } from 'react-router-dom';
 import Navbar from './Components/Navbar.jsx'
 import Main from './Components/Main.jsx'
 import Astros from './Components/Astros.jsx'
+import Ejercicio from './Components/Ejercicio.jsx';
 import './App.css'
 
 function App() {
 
     const linksData = [
         { name: 'Home', href: '/'},
-        { name: 'About', href: '/'},
+        { name: 'About', href: '/ejercicio'},
         { name: 'Astros', href: '/astros'},
     ]
 
@@ -24,6 +25,10 @@ function App() {
                     element={<Astros />}
                     path='/astros'
                 />
+                <Route 
+                    element={<Ejercicio />}
+                    path='/ejercicio'
+                />    
             </Routes>
         </>
     )
---
+++ src/Components/Ejercicio.jsx
@@ -0,0 +1,38 @@
+
+import React from "react";
+
+const Ejercicio = () => {
+  return (
+    <div>
+      <h2>Sobre mi</h2>
+      
+  
+      <p>
+        <strong>Luis Enrique Flemate Yañez &#128187; &#128640;</strong> 
+      </p>
+      <p>
+        <strong>Correo:</strong> 
+      </p>
+
+      <p>luisenriqueflemateyanez@gmail.com</p>
+      <p>
+        <strong>Descripción:</strong>
+      </p>
+      <p>Hola estoy en <b>Generation Mexico</b> aprendiendo a programar.<br/>
+      Soy un apasionado por la musica aunque no se tocar instrumentacion &#128517; me gusta mucho escuhcar musica<br/>
+      por que me relaja y me da mas conifianza en seguir con planes o trabajos.<br/>
+      Tambien me gusta demasiado la investigacion y temas relaciones con geografia y geo-politica &#128506;
+      </p>
+
+        <p>
+          <strong>Links:</strong>
+          </p>
+
+          <p><a href="https://github.com/k1k3fy">Github</a></p>
+          <p><a href="https://www.linkedin.com/in/luis-enrique-flemate-ya%C3%B1ez/">Linkedin</a></p>
+
+    </div>
+  );
+};
+
+export default Ejercicio;
